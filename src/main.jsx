216ce0c89b1a7b01c3737a307ecae4c6c55025af
crespiramiro <crespiramiro@outlook.com>
2024-10-05 00:03:57 -0300

    vercel-analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { inject } from '@vercel/analytics/*'
 
 createRoot(document.getElementById('root')).render(
+
+  inject(),
   <StrictMode>
     <App />
   </StrictMode>,
